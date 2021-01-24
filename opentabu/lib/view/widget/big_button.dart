d2fd8a2e845c646a38d41de8159de8b597e23622
Leo <dev.rignaneseleo@gmail.com>
2021-01-24 19:48:47 +0100

    Update big_button.dart

--- opentabu/lib/view/widget/big_button.dart
+++ opentabu/lib/view/widget/big_button.dart
@@ -17,12 +17,16 @@ class BigButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.only(top: 20),
-      child: MaterialButton(elevation: 0,
+      child: MaterialButton(
+          elevation: 0,
           minWidth: double.infinity,
           height: 90,
           child: UpperCaseText(
             text,
-            style: Theme.of(context).textTheme.headline2.copyWith(color: textColor),
+            style: Theme.of(context)
+                .textTheme
+                .headline2
+                .copyWith(color: textColor),
           ),
           color: bgColor,
           shape:
@@ -36,9 +40,10 @@ class BigIconButton extends StatelessWidget {
   final Color bgColor;
   final VoidCallback onPressed;
   final Image icon;
+  final double size;
 
   const BigIconButton(
-      {Key key, this.bgColor, this.onPressed, this.icon})
+      {Key key, this.bgColor, this.onPressed, this.icon, this.size = 90})
       : super(key: key);
 
   @override
@@ -47,7 +52,7 @@ class BigIconButton extends StatelessWidget {
       margin: EdgeInsets.only(top: 20),
       child: MaterialButton(
           minWidth: double.infinity,
-          height: 90,
+          height: size,
           child: icon,
           color: bgColor,
           shape:
