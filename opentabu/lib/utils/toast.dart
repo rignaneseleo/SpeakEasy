30d690c6242c9389887a18be7814f337b3e8e312
Leo <dev.rignaneseleo@gmail.com>
2021-05-06 18:27:35 +0200

    improved info page

---
+++ opentabu/lib/utils/toast.dart
@@ -0,0 +1,12 @@
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:opentabu/theme/theme.dart';
+
+void showToast(String text) => Fluttertoast.showToast(
+      msg: text,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIosWeb: 1,
+      backgroundColor: darkPurple,
+      textColor: txtWhite,
+      fontSize: 16.0,
+    );
--- opentabu/lib/view/info_page.dart
+++ opentabu/lib/view/info_page.dart
@@ -1,6 +1,8 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:opentabu/theme/theme.dart';
+import 'package:opentabu/utils/toast.dart';
 import 'package:opentabu/view/widget/my_scaffold.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -12,16 +14,52 @@ class InfoPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return MyScaffold(widgets: [
       //new Text("VERSIONE ${Weco.AppVersion}"),
-      /*Container(
-        height: 200,
-        child: TextButton(
-          onPressed: () => _launchURL("mailto:$emailLeo?subject=Bug%20tabu%20"),
-          child: new Text(
-            "Report BUG",
-            style: Theme.of(context).textTheme.headline4.copyWith(color: myRed),
-          ),
+      Expanded(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical:20.0),
+              child: IconButton(
+                  icon: Icon(
+                    Icons.arrow_back_ios_rounded,
+                    color: Theme.of(context).canvasColor,
+                  ),
+                  onPressed: () => Get.back()),
+            ),
+            Container(height: 60),
+            TextButton(
+              onPressed: () => showToast("Soon available, but thanks! :)"),
+              child: new Text(
+                "Donate",
+                style: Theme.of(context)
+                    .textTheme
+                    .headline4
+                    .copyWith(color: myYellow),
+              ),
+            ),
+            TextButton(
+              onPressed: () => showToast("Stay tuned ;)"),
+              child: new Text(
+                "Analytics",
+                style: Theme.of(context).textTheme.headline4,
+              ),
+            ),
+            TextButton(
+              onPressed: () =>
+                  _launchURL("mailto:$emailLeo?subject=Bug%20tabu%20"),
+              child: new Text(
+                "Report BUG",
+                style: Theme.of(context)
+                    .textTheme
+                    .headline4
+                    .copyWith(color: txtGrey),
+              ),
+            ),
+          ],
         ),
-      ),*/
+      ),
       GestureDetector(
         child: new AutoSizeText(
           "Made by\nLeonardo Rignanese",
