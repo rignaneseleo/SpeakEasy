a3740c73bbdd1153791704bc721d92634e05f405
Leo <dev.rignaneseleo@gmail.com>
2021-08-27 10:01:51 +0200

    Revert "Fixed bug app not loading with en-US"
    This reverts commit d9923ab6e9a8db1c281c8db42ec4b8562c8c9b43.

--- opentabu/lib/main.dart
+++ opentabu/lib/main.dart
@@ -6,7 +6,6 @@
 import 'dart:async';
 
 import 'package:easy_localization/easy_localization.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/all.dart';
@@ -37,7 +36,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
   InAppPurchaseConnection.enablePendingPurchases();
-  await Firebase.initializeApp();
+
 
   // Turn off landscape mode
   await SystemChrome.setPreferredOrientations(
