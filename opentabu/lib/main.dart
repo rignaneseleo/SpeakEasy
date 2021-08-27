d9923ab6e9a8db1c281c8db42ec4b8562c8c9b43
Leo <dev.rignaneseleo@gmail.com>
2021-08-27 09:46:29 +0200

    Fixed bug app not loading with en-US

--- opentabu/lib/main.dart
+++ opentabu/lib/main.dart
@@ -6,6 +6,7 @@
 import 'dart:async';
 
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/all.dart';
@@ -36,7 +37,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
   InAppPurchaseConnection.enablePendingPurchases();
-
+  await Firebase.initializeApp();
 
   // Turn off landscape mode
   await SystemChrome.setPreferredOrientations(
@@ -63,10 +64,11 @@ Future<void> main() async {
   runApp(
     ProviderScope(
       child: EasyLocalization(
-          supportedLocales: [Locale('en'), Locale('it')],
-          path: 'assets/lang',
-          fallbackLocale: Locale('en', 'US'),
-          child: OpenTabuApp()),
+        supportedLocales: [Locale('en'), Locale('it')],
+        path: 'assets/lang',
+        fallbackLocale: Locale('en'),
+        child: OpenTabuApp(),
+      ),
     ),
   );
 }
